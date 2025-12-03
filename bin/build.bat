d445156a4087b77708bf10825c20f156a859c7a4
YUSUKE IZAWA <me@yizawa.com>
2025-12-03 17:49:06 +0900

    Update build.bat

--- bin/build.bat
+++ bin/build.bat
@@ -8,4 +8,4 @@ set ocamlc=.\win64ocaml\bin\ocamlc.exe
 %ocamlc% -c .\syntax.ml
 %ocamlyacc% .\parser.mly
 %ocamllex% .\lexer.mll
-%ocamlc% .\syntax.ml .\parser.mli .\parser.ml .\lexer.ml .\virtual_stack.ml .\emit_wasm.ml .\main.ml -o .\while_lang
+%ocamlc% .\syntax.ml .\error.ml .\parser.mli .\parser.ml .\lexer.ml .\virtual_stack.ml .\emit_wasm.ml .\visualizer.ml .\main.ml -o .\while_lang
