f484fec2abdffb72d32af744f35683215480053a
YUSUKE IZAWA <me@yizawa.com>
2025-12-18 16:12:54 +0900

    Fix day2.bat

--- day2.bat
+++ day2.bat
@@ -6,8 +6,9 @@ set ocamllex=.\win64ocaml\bin\ocamllex.exe
 set ocamlc=.\win64ocaml\bin\ocamlc.exe
 
 %ocamlc% -c .\syntax.ml
+%ocamlc% -c .\error.ml
 %ocamlyacc% .\parser.mly
 %ocamllex% .\lexer.mll
 %ocamlc% -c .\*.mli
-%ocamlc% .\syntax.ml .\parser.ml .\lexer.ml .\virtual_stack.ml .\test_day2.ml -o .\test_day2
+%ocamlc% .\syntax.ml .\error.ml .\parser.ml .\lexer.ml .\virtual_stack.ml .\visualizer.ml .\test_day2.ml -o .\test_day2
 .\bin\run.bat .\test_day2
